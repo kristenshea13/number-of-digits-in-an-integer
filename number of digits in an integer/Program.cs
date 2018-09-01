e5f26e438071e32b2caf35136a817a96780d7ee2
Kristen Shea <kstar61@live.com>
2018-08-31 21:00:58 -0500

    method to determine number of digits in a user-entered integer.

--- number of digits in an integer/Program.cs
+++ number of digits in an integer/Program.cs
@@ -6,7 +6,31 @@ namespace number_of_digits_in_an_integer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Write a method that will return the number of digits in an integer.
+            Console.WriteLine("How many digits in a value?");
+            Console.Write("Enter number: ");
+
+            //convert number entered from string to int and declare it as a variable
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The number of digits in value: {0}", Digits(number));
+
+            Console.ReadLine();
+
         }
+
+        /// <summary>
+        /// return the number of digits in the user-entered integer from above
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>number of digits</returns>
+        private static int Digits(int number)
+        {
+            return number.ToString().Length;
+        }
+
+
+
+
     }
 }
