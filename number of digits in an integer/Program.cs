6a27c8cb7af4662df3d4551c4df602d557d4ddc8
Kristen Shea <kstar61@live.com>
2018-08-31 20:48:04 -0500

    Add project files.

---
+++ number of digits in an integer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace number_of_digits_in_an_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
